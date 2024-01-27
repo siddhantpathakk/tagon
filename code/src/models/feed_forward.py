c7bcc311be8f155e437d4c5c4005599203df90ac
#PATHAK SIDDHANT# <siddhant005@SCSEGPU-TC1.cm.cluster>
2024-01-27 21:38:20 +0800

    add: pointwise FFN layer

---
+++ code/src/models/feed_forward.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+class PointWiseFeedForward(nn.Module):
+    def __init__(self, hidden_units, dropout_rate): # wried, why fusion X 2?
+
+        super(PointWiseFeedForward, self).__init__()
+
+        self.conv1 = nn.Conv1d(hidden_units, hidden_units, kernel_size=1)
+        self.dropout1 = nn.Dropout(p=dropout_rate)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv1d(hidden_units, hidden_units, kernel_size=1)
+        self.dropout2 = nn.Dropout(p=dropout_rate)
+
+    def forward(self, inputs):
+        outputs = self.dropout2(self.conv2(self.relu(self.dropout1(self.conv1(inputs.transpose(-1, -2))))))
+        outputs = outputs.transpose(-1, -2) # as Conv1D requires (N, C, Length)
+        outputs += inputs
+        return outputs
