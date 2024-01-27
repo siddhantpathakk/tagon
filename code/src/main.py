a11f9c9fab68257659c21251e1e0e51922efe288
#PATHAK SIDDHANT# <siddhant005@SCSEGPU-TC1.cm.cluster>
2024-01-27 21:39:03 +0800

    edit: arg parser improved

--- code/src/main.py
+++ code/src/main.py
@@ -3,43 +3,105 @@ import torch
 import os
 from dataloader.dataloader import DataCollector
 from trainer import Trainer
+import json
+import numpy as np
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def seed_everything(seed=42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
+    np.random.seed(seed)
 
 def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file_path', type=str, default='/home/FYP/siddhant005/fyp/code/data/processed/ml-100k/')
+    parser = argparse.ArgumentParser(description='Trainer for FYP GNN')
+    
+    # directory based parameters
+    parser.add_argument('--dataset', type=str, default='ml100k')
     parser.add_argument('--out_path', type=str, default='/home/FYP/siddhant005/fyp/code/src/logs/tmp/')
+
+    # data based parameters
     parser.add_argument('--L', type=int, default=11)
     parser.add_argument('--H', type=int, default=2)
-    parser.add_argument('--epoch_num', type=int, default=100)
     parser.add_argument('--topk', type=int, default=20)
-    parser.add_argument('--batch_size', type=int, default=256)
-    parser.add_argument('--lambda_val', type=float, default=0.5)
-    parser.add_argument('--learning_rate', type=float, default=4e-3)
-    parser.add_argument('--l2', type=float, default=1e-4)
+    
+    # model training based parameters
+    parser.add_argument('--epoch_num', type=int, default=100)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--l2', type=float, default=1e-5)
+    
+    # negative sampling parameters
     parser.add_argument('--neg_samples', type=int, default=3)
     parser.add_argument('--sets_of_neg_samples', type=int, default=50)
+    
+    # GCN based parameters
     parser.add_argument('--dim', type=int, default=32)
-    parser.add_argument('--conv_layer_num', type=int, default=3)
-    parser.add_argument('--adj_dropout', type=int, default=0)
+    parser.add_argument('--conv_layer_num', type=int, default=2)
+    parser.add_argument('--short_term_conv_layer_num', type=int, default=3)
     parser.add_argument('--num_bases', type=int, default=2)
-    parser.add_argument('--verbose', type=bool, default=True)
+    parser.add_argument('--adj_dropout', type=int, default=0)
+    parser.add_argument('--lambda_val', type=float, default=0.5)
+
+    # TSAL based parameters
+    parser.add_argument('--TSAL_head_num', type=int, default=2)
+    parser.add_argument('--TSAL_attn_drop', type=float, default=0.0)
+    
+    # User Attention based parameters
+    parser.add_argument('--user_attn_head_num', type=int, default=2)
+    parser.add_argument('--user_attn_drop', type=float, default=0.0)
+    
+    # other parameters
+    parser.add_argument('--verbose', type=str2bool, nargs='?', const=True, default=False)
+    parser.add_argument('--debug', type=str2bool, nargs='?', const=True, default=False)
+    parser.add_argument('--seed', type=int, default=42)
     return parser.parse_args()
 
 if __name__ == '__main__':
 
     config = parse_opt()
-    print(f'[STATUS] Running train.py with arguments: {config}')
-    print('=='*55)
+    
+    if config.dataset == 'ml100k':
+        config.file_path = '/home/FYP/siddhant005/fyp/code/data/processed/ml-100k/'
+        config.batch_size = 256
+    elif config.dataset == 'ml1m':
+        config.file_path = '/home/FYP/siddhant005/fyp/code/data/processed/ml-1m/'
+        config.batch_size = 512
+    else:
+        raise NotImplementedError(f'[ERROR] Dataset {config.dataset} not implemented')
+
+    seed_everything(config.seed)
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f'[INFO] device: {device} ')
-    print('=='*55)
+    
+    if config.debug:
+        config.epoch_num = 10
+        # config.verbose = True
+        print(config.__dict__)
+        
+    with open(config.out_path+'commandline_args.txt', 'w') as f:
+        json.dump(config.__dict__, f, indent=2)
     
     datacollector = DataCollector(config)
     uid2locid_time,locid2detail,node_num,relation_num,uid_list_,user_np,seq_train,seq_test,test_set,u2v,u2vc,v2u,v2vc = datacollector.main(save1=True,save2=True)
     
-    print(f'[STATUS] Data preprocessing completed')
     if config.verbose:
+        print(f'[STATUS] Running train.py with arguments: {config}')
+        print('=='*100)
+        print(f'[INFO] device: {device} ')
+        print('=='*100)
+        print(f'[STATUS] Data preprocessing completed')
         print(f'[INFO] uid2locid_time: {len(uid2locid_time)} ')
         print(f'[INFO] locid2detail: {len(locid2detail)} ')
         print(f'[INFO] node_num: {node_num} ')
@@ -54,15 +116,12 @@ if __name__ == '__main__':
         print(f'[INFO] u2vc: {len(u2vc)} ')
         print(f'[INFO] v2u: {len(v2u)} ')
         print(f'[INFO] v2vc: {len(v2vc)} ')
-    print('=='*55)
-    
-    modeling = Trainer(config,node_num,relation_num,u2v,u2vc,v2u,v2vc,device)
+        print('=='*100)
+        print(f'[STATUS] Commencing training for {config.epoch_num} epochs\n')
     
     train_part = [user_np,seq_train]
     test_part = [seq_test,test_set,uid_list_,uid2locid_time]
     
-    print(f'[STATUS] Commencing training for {config.epoch_num} epochs\n')
-    modeling.train(train_part,test_part)
-    print('=='*55)
-    print('[STATUS] Training completed successfully')
+    modeling = Trainer(config,node_num,relation_num,u2v,u2vc,v2u,v2vc,device)
     
+    modeling.train(train_part,test_part)
