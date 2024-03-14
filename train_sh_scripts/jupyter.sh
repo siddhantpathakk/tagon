75963b83cfc670a7f745c9db45677a00f1fd1ae5
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2024-03-14 16:49:30 +0800

    add: sh scripts

---
+++ train_sh_scripts/jupyter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##Job Script for FYP
+
+#SBATCH --partition=SCSEGPU_UG
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --qos=q_ug24
+#SBATCH --mem=32G
+#SBATCH --job-name=TGAT_Jupyter
+#SBATCH --output=/home/FYP/siddhant005/fyp/train_sh_scripts/slurm_logs/output_%x_%j.out
+#SBATCH --error=/home/FYP/siddhant005/fyp/train_sh_scripts/slurm_logs/error_%x_%j.err
+
+module load anaconda
+module load cuda/12.1
+source activate retagnn_pyg
+which python
+jupyter-notebook --ip=$(hostname -i) --port=8885
---
+++ train_sh_scripts/train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##Job Script for FYP
+
+#SBATCH --partition=SCSEGPU_UG
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --qos=q_ug24
+#SBATCH --mem=32G
+#SBATCH --job-name=TGSRec
+#SBATCH --output=/home/FYP/siddhant005/fyp/train_sh_scripts/slurm_logs/train/output_%x_%j.out
+#SBATCH --error=/home/FYP/siddhant005/fyp/train_sh_scripts/slurm_logs/train/error_%x_%j.err
+
+module load anaconda
+module load cuda/12.1
+source activate retagnn_pyg
+which python
+python /home/FYP/siddhant005/fyp/src/main.py -d ml-100k --uniform --bs 256
