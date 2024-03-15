57332b326aca06bcc216afa951d4b37f4aad43f4
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2024-03-15 15:39:11 +0800

    Accelerate evaluation

--- src/utils/evaluation_utils.py
+++ src/utils/evaluation_utils.py
@@ -114,13 +114,13 @@ def mean_reciprocal_rank(r):
 
 Ks = [10, 20]
 
-def eval_one_user(x):
-    logger = logging.getLogger()
-    
+def eval_one_user(x):    
+    # print('eval_one_user')
     result = {'precision': np.zeros(len(Ks)), 
               'recall': np.zeros(len(Ks)), 
               'ndcg': np.zeros(len(Ks)),
                 'hit_ratio': np.zeros(len(Ks)), 'auc': 0., 'mrr': 0.}
+    
     preds = np.transpose(x[0])
 
     pos_label = np.ones(1)
@@ -202,7 +202,7 @@ def eval_users(tgrec, src, dst, ts, train_src, train_dst, args):
             train_pos_edges[u] = set([i])
 
     pool = multiprocessing.Pool(cores)
-    batch_users = 5
+    batch_users = 1000
 
     preds_list = []
     preds_len_preditems = []
@@ -220,9 +220,9 @@ def eval_users(tgrec, src, dst, ts, train_src, train_dst, args):
         batch_src_l = []
         batch_test_items = []
         batch_ts = []
-        #batch_len = []
         batch_i = 0
-        for idx2, (u, i, t) in enumerate(zip(src, dst, ts)):
+        
+        for _, (u, i, t) in enumerate(zip(src, dst, ts)):
             
             num_test_instances += 1
             if u not in train_src or i not in train_itemset or u not in pos_edges:
@@ -241,6 +241,10 @@ def eval_users(tgrec, src, dst, ts, train_src, train_dst, args):
             if args.negsampleeval == -1:
                 neg_items = neg_candidates
             else:
+                # print('len train_itemset', len(train_itemset))
+                # print('len pos_items', len(set(pos_items)))
+                # print('len interacted_dst', len(interacted_dst))
+                # print('len neg_cand', len(neg_candidates))
                 neg_items = list(np.random.choice(neg_candidates, size=args.negsampleeval, replace=False))
             #neg_items = list(train_itemset - set(pos_items))
             neg_ts = [t for _ in range(len(neg_items))]
@@ -304,43 +308,6 @@ def eval_users(tgrec, src, dst, ts, train_src, train_dst, args):
                 batch_src_l = []
                 batch_test_items = []
                 batch_ts = []
-                #batch_len = []
-            #pred_prob = pred_scores.sigmoid()
-
-            #neg_label = np.zeros(len(neg_items))
-
-            #preds = pred_scores.cpu().numpy()
-            #preds_list.append(preds)
-            #preds_len_preditems.append(len(neg_items)+1)
-            #scaler = MinMaxScaler()
-            #posterior = np.transpose(scaler.fit_transform(np.transpose([preds])))[0]
-            #posterior = -np.sort(-posterior)
-            ##posterior = pred_prob.cpu().numpy()
-            #labels = np.concatenate([pos_label, neg_label])
-
-            #r = []
-            #rankeditems = list((-preds).argsort())
-            #for i in rankeditems:
-            #    if i == 0:
-            #        r.append(1)
-            #    else:
-            #        r.append(0)
-
-            #precision, recall, ndcg, hit_ratio = [], [], [], []
-            #for K in Ks:
-            #    precision.append(metrics.precision_at_k(r, K))
-            #    recall.append(metrics.recall_at_k(r, K, len(pos_items)))
-            #    ndcg.append(metrics.ndcg_at_k(r, K))
-            #    hit_ratio.append(metrics.hit_at_k(r, K))
-            #auc = metrics.auc(ground_truth=r, prediction=posterior)
-            #mrr = metrics.mrr(r)
-
-            #result['precision'] += precision
-            #result['recall'] += recall
-            #result['ndcg'] += ndcg
-            #result['hit_ratio'] += hit_ratio
-            #result['auc'] += auc
-            #result['mrr'] += mrr
 
 
     result['precision'] /= num_interactions
@@ -349,10 +316,97 @@ def eval_users(tgrec, src, dst, ts, train_src, train_dst, args):
     result['hit_ratio'] /= num_interactions
     result['auc'] /= num_interactions
     result['mrr'] /= num_interactions
-    print('num_interactions: ', num_interactions)
 
     return result, test_outputs
 
+
+def eval_users_withoutMP(tgrec, src, dst, ts, train_src, train_dst, args):
+    logger = logging.getLogger()
+    result = {'precision': np.zeros(len(Ks)), 'recall': np.zeros(len(Ks)), 'ndcg': np.zeros(len(Ks)),
+              'hit_ratio': np.zeros(len(Ks)), 'auc': 0., 'mrr': 0.}
+    userset = set(src)
+    train_itemset = set(train_dst)
+    pos_edges = {}
+    for u, i, t in zip(src, dst, ts):
+        if i not in train_itemset:
+            continue
+        if u in pos_edges:
+            pos_edges[u].add((i, t))
+        else:
+            pos_edges[u] = set([(i, t)])
+    train_pos_edges = {}
+    for u, i in zip(train_src, train_dst):
+        if u in train_pos_edges:
+            train_pos_edges[u].add(i)
+        else:
+            train_pos_edges[u] = set([i])
+
+    test_outputs = []
+    num_interactions = 0
+    num_test_instances = 0
+    
+    with torch.no_grad():
+        tgrec = tgrec.eval()
+        
+        for _, (u, i, t) in enumerate(zip(src, dst, ts)):
+            num_test_instances += 1
+            if u not in train_src or i not in train_itemset or u not in pos_edges:
+                continue
+            num_interactions += 1
+
+            pos_items = [i]
+            pos_ts = [t]
+            src_l = [u for _ in range(len(pos_items))]
+            pos_label = np.ones(len(pos_items))
+
+            interacted_dst = train_pos_edges[u]
+
+            neg_candidates = list(train_itemset - set(pos_items) - interacted_dst)
+            if args.negsampleeval == -1:
+                neg_items = neg_candidates
+            else:
+                neg_items = list(np.random.choice(neg_candidates, size=args.negsampleeval, replace=False))
+
+            neg_ts = [t for _ in range(len(neg_items))]
+            neg_src_l = [u for _ in range(len(neg_items))]
+
+            batch_src_l = src_l + neg_src_l
+            batch_test_items = pos_items + neg_items
+            batch_ts = pos_ts + neg_ts
+
+            test_items = np.array(batch_test_items)
+            test_ts = np.array(batch_ts)
+            test_src_l = np.array(batch_src_l)
+
+            pred_scores = tgrec(test_src_l, test_items, test_ts, args.n_degree)
+            preds = pred_scores.cpu().numpy()
+
+            # Evaluate predictions for the current user
+            eval_result = eval_one_user((preds, len(batch_src_l), (u, i, t), batch_test_items, args.negsampleeval, len(pos_items+neg_candidates)))
+            re = eval_result[0]
+            result['precision'] += re['precision']
+            result['recall'] += re['recall']
+            result['ndcg'] += re['ndcg']
+            result['hit_ratio'] += re['hit_ratio']
+            result['auc'] += re['auc']
+            result['mrr'] += re['mrr']
+
+            uit = eval_result[2]
+            pred_rank_list = eval_result[1]
+            rec_items = eval_result[3]
+
+            one_pred_result = {"u_ind": int(uit[0]), "u_pos_gd": int(uit[1]), "timestamp": float(uit[2])}
+            one_pred_result["predicted"] = [int(rec_items[int(rec_ind)]) for rec_ind in pred_rank_list]
+            test_outputs.append(one_pred_result)
+
+    result['precision'] /= num_interactions
+    result['recall'] /= num_interactions
+    result['ndcg'] /= num_interactions
+    result['hit_ratio'] /= num_interactions
+    result['auc'] /= num_interactions
+    result['mrr'] /= num_interactions
+
+    return result, test_outputs
 
 def eval_one_epoch(hint, tgrec, sampler, src, dst, ts, label, NUM_NEIGHBORS=20):
     val_acc, val_ap, val_f1, val_auc = [], [], [], []
