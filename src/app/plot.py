f2203920d7c576eaf3c82164eb66c280d4451bf6
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2024-05-08 03:09:21 +0800

    Add sublabel to nodes

--- src/app/plot.py
+++ src/app/plot.py
@@ -9,21 +9,17 @@ def make_ctbg(user_hist, dataset, trim=10):
     user_hist = user_hist.sort_values(by='ts', ascending=False).head(trim)
     user_hist['ts'] = pd.to_datetime(user_hist['ts'], unit='s')
 
-    print(user_hist[['u', 'i', 'ts']])
-
     # Load the item mapping file
     map_file_path = f'/Users/siddhantpathak/Desktop/Projects/tagon/processed/{dataset}_i_map.json'
     item_name_map = json.load(open(map_file_path))
-
-    # ignore
     item_name_df = pd.DataFrame(item_name_map).T
     print(item_name_df)
-
+    
     # Create a directed bipartite graph
     B = nx.DiGraph()
     B.add_nodes_from(user_hist['u'].unique(), bipartite=0)
     B.add_nodes_from(user_hist['i'].unique(), bipartite=1)
-    edges = user_hist.apply(lambda row: B.add_edge(row['u'], row['i']), axis=1)
+    user_hist.apply(lambda row: B.add_edge(row['u'], row['i']), axis=1)
 
     # Generate positions for the nodes
     pos = nx.bipartite_layout(B, user_hist['u'].unique())
@@ -35,13 +31,16 @@ def make_ctbg(user_hist, dataset, trim=10):
         if node in y_pos:
             pos[node] = (position[0], y_pos[node])
 
-    # Center the user node by adjusting the y-position
-    item_y_positions = [pos[node][1] for node in sorted_items]
-    if item_y_positions:
-        mid_y = (max(item_y_positions) + min(item_y_positions)) / 2
-    else:
-        mid_y = 0
+    # Assign labels to item nodes with subscript formatting
+    item_labels = {item: f"i<sub>{index + 1}</sub>" for index,
+                   item in enumerate(sorted_items)}
+    user_labels = {user: f"u<sub>{index + 1}</sub>" for index,
+                   user in enumerate(user_hist['u'].unique())}
 
+    # Adjust the y-position of the user nodes to be centered
+    item_y_positions = [pos[node][1] for node in sorted_items]
+    mid_y = (max(item_y_positions) + min(item_y_positions)) / \
+        2 if item_y_positions else 0
     for user in user_hist['u'].unique():
         pos[user] = (pos[user][0], mid_y)
 
@@ -54,44 +53,41 @@ def make_ctbg(user_hist, dataset, trim=10):
         edge_x.extend([x0, x1, None])
         edge_y.extend([y0, y1, None])
 
-        arrows.append(dict(
-            x=x1, y=y1, xref='x', yref='y',
-            ax=x0, ay=y0, axref='x', ayref='y',
-            showarrow=True, arrowhead=1, arrowsize=0.8, arrowwidth=1, arrowcolor='darkslategrey'))
-
     node_x = [pos[node][0] for node in B.nodes()]
     node_y = [pos[node][1] for node in B.nodes()]
-    node_colors = ['white' if node in user_hist['u'].unique()
-                   else '#FFD700' for node in B.nodes()]
+    node_colors = ['#A4D4B4' if node in user_hist['u'].unique()
+                   else '#E4D9FF' for node in B.nodes()]
 
+    # Node labels for item and user nodes with subscript
+    node_texts = [user_labels.get(node, item_labels.get(node, ""))
+                  for node in B.nodes()]
+
+    # Hover texts including item and user details
     hover_texts = [
         f"User ID: {node}" if node in user_hist['u'].unique() else
-        f"<b>Timestamp</b>: {user_hist[user_hist['i'] == node]['ts'].iloc[0]}<br><br><b>Item ID</b>: {node}<br><b>Item Name</b>: {get_item_name(item_name_df, node)[0]}<br><b>Year</b>: {get_item_name(item_name_df, node)[1]}"
+        f"Item ID: {node}<br>Item Name: {get_item_name(item_name_df, node)}<br>Timestamp: {user_hist[user_hist['i'] == node]['ts'].iloc[0]}"
         for node in B.nodes()
     ]
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=edge_x, y=edge_y, mode='lines', line=dict(
-        width=0.5, color='darkslategrey'), hoverinfo='none'))
-    fig.add_trace(go.Scatter(x=node_x, y=node_y, mode='markers', marker=dict(size=18, color=node_colors),
-                             text=hover_texts, hoverinfo='text', textposition='bottom left',
-                             hoverlabel=dict(bgcolor='white', bordercolor=node_colors,
-                                                                                 font=dict(color='black', family='Open Sans, sans-serif', size=14)))
-    )
+        width=0.5, color='#e50914'), hoverinfo='none'))
+    fig.add_trace(go.Scatter(x=node_x, y=node_y, mode='markers+text', text=node_texts, marker=dict(size=30, color=node_colors),
+                             hoverinfo='text', hovertext=hover_texts, textposition='top center',
+                             hoverlabel=dict(bgcolor='white',
+                                             bordercolor=node_colors,
+                                             font=dict(color='black', family='Open Sans, sans-serif', size=14)),
+                             textfont=dict(size=16)))
 
     fig.update_layout(showlegend=False, xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                      yaxis=dict(showgrid=False, zeroline=False, showticklabels=False), margin=dict(l=0, r=0, t=0, b=0), annotations=arrows)
+                      yaxis=dict(showgrid=False, zeroline=False, showticklabels=False), margin=dict(l=0, r=0, t=0, b=0),
+                      annotations=arrows)
 
     return fig
 
 def get_item_name(item_df, item_id):
     name = item_df[item_df['item_id'] == item_id]['title'].iloc[0] if item_id in item_df['item_id'].values else 'UNKNOWN ITEM'
-    if name == 'UNKNOWN ITEM':
-        return name, "???"
-    else:
-        name = name.split("(")
-        item_name, year_str = name[0].strip(), name[1].replace(")", "").strip()
-        return item_name, year_str
+    return name
 
 def make_recgraph(ctbg, rec_output):
     pass
