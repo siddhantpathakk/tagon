ab9f322b4a99b260b3062ae30bbda4634dd7aae5
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2024-05-08 03:09:36 +0800

    Sidebar settings

--- src/app/app.py
+++ src/app/app.py
@@ -20,7 +20,6 @@ def get_output(dataset, user_id):
     NUM_HEADS = args.n_head
     DROP_OUT = args.drop_out
     GPU = args.gpu
-    NEW_NODE = args.new_node
     USE_TIME = args.time
     AGG_METHOD = args.agg_method
     ATTN_MODE = args.attn_mode
@@ -30,7 +29,6 @@ def get_output(dataset, user_id):
     LEARNING_RATE = args.lr
     NODE_DIM = args.node_dim
     TIME_DIM = args.time_dim
-    test_mode = False
     infer_mode = True 
     USER_ID = user_id
 
@@ -68,7 +66,7 @@ def get_output(dataset, user_id):
 # Streamlit UI
 st.set_page_config(page_title='TAGON',
                    page_icon=":material/sdk:",
-                    layout='wide',
+                    layout='centered',
                     initial_sidebar_state='expanded',
                     menu_items={
                         "Get Help": "https://www.github.com/siddhantpathakk/tagon",
@@ -80,30 +78,44 @@ st.title(f'FYP Demonstration for TAGON')
 
 dataset_names = ['ml-100k', 'Baby', 'Digital_Music', 'Toys_and_Games', 'Tools_and_Home_Improvement']
 
+# Sidebar
+st.sidebar.title("Settings")
+
+st.sidebar.subheader("Select dataset and user ID")
 selected_dataset = st.sidebar.selectbox("Select dataset", dataset_names, placeholder="Select a dataset")
-user_id = st.sidebar.text_input("Enter User ID", "0")
-trim = st.sidebar.slider("Trim (for CTBG)", 1, 10, 9)
+user_id = st.sidebar.text_input("Enter User ID", "2")
+user_id = int(user_id)
+
+
+st.sidebar.subheader("CTBG Settings")
+trim = st.sidebar.slider("Trim (for CTBG)", 1, 6, 4)
+show_csv = st.sidebar.checkbox("Show CSV Data")
 
 session_state = False
-if st.button('Predict'):
-    user_id = int(user_id) 
-
+st.sidebar.subheader("Make Predictions")
+if st.sidebar.button('Predict', type='primary'):
     print(f"Selected dataset: {selected_dataset}")
     print(f"User ID: {user_id}")
     user_hist, output = get_output(selected_dataset, user_id)
-    st.toast("Predictions completed!",  icon="✅")
     session_state = True
+    st.toast("Predictions completed!",  icon="✅")
     
 if session_state:
+    
     st.header("Continuous Time Bipartite Graph (CTBG)")
     fig = make_ctbg(user_hist, selected_dataset, trim=trim)
     st.plotly_chart(fig)
         
-    # if st.checkbox("CSV Data"):
-    # dataframe = pd.DataFrame(user_hist)
-    # st.dataframe(dataframe)
+        
+    if show_csv:
+        dataframe = pd.DataFrame(user_hist)
+        st.dataframe(dataframe)
 
     st.header("Recommendations")
-    st.subheader("CSV Data")
-    dataframe = pd.DataFrame(output)
-    st.dataframe(dataframe)
+    if show_csv:
+        st.subheader("CSV Data")
+        dataframe = pd.DataFrame(output)
+        dataframe = dataframe.sort_values(by='timestamp', ascending=False)
+        dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'], unit='s')
+        dataframe = dataframe[['u_pos_gd', 'timestamp', 'predicted']][::-1].reset_index(drop=True)
+        st.dataframe(dataframe)
