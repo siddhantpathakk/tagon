3e12c7a2c975eb23cb4fa37321f8b975ae66010b
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2024-05-10 12:54:05 +0800

    Add state management + UI fix

--- src/app/app.py
+++ src/app/app.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 import pandas as pd
 
-from plot import make_ctbg, make_recgraph
+from plot import make_ctbg, make_plotly_table, make_recgraph
 from src.components.trainer.trainer import Trainer
 from src.components.utils.parse import parse_training_args
 from src.components.trainer.trainer_utils import setup_model, setup_optimizer
@@ -11,13 +11,11 @@ from src.components.utils.utils import EarlyStopMonitor, set_seed
 from src.components.data.data import Data
 from src.components.utils.consts import *
 
-import warnings
-warnings.filterwarnings("ignore")
 
-
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def get_output(dataset, user_id):
     args = parse_training_args(dataset=dataset)
+    DATASET = args.data
 
     BATCH_SIZE = args.bs
     NUM_NEIGHBORS = args.n_degree
@@ -29,11 +27,10 @@ def get_output(dataset, user_id):
     AGG_METHOD = args.agg_method
     ATTN_MODE = args.attn_mode
     SEQ_LEN = NUM_NEIGHBORS
-    DATASET = args.data
     NUM_LAYER = args.n_layer
     LEARNING_RATE = args.lr
     NODE_DIM = args.node_dim
-    TIME_DIM = args.time_dim
+    TIME_DIM = args.time_dim if DATASET != 'ml-100k' else 8
     infer_mode = True 
     USER_ID = user_id
 
@@ -42,15 +39,12 @@ def get_output(dataset, user_id):
     set_seed(args.seed)
 
     data = Data(DATASET, args)
-    n_nodes = data.max_idx
-    n_edges = data.num_total_edges
-    print('Number of nodes:', n_nodes)
-    print('Number of edges:', n_edges)
 
     user_history = data.get_user_history(USER_ID)
-    print('User history', '\n', user_history.head())    
 
-    pretrain = pretrain_app_path_slab(args, cwd)
+    # pretrain = pretrain_app_path_scse(args, cwd)
+    # pretrain = pretrain_app_path_slab(args, cwd)
+    pretrain = pretrain_app_path_new(args, cwd) if DATASET != 'ml-100k' else pretrain_ml100k_slab(args, cwd)
 
     model = setup_model(data, args, data.max_idx, GPU, NUM_LAYER, USE_TIME, AGG_METHOD, ATTN_MODE, SEQ_LEN, NUM_HEADS, DROP_OUT, NODE_DIM, TIME_DIM,
                         load_pretrain=pretrain)
@@ -61,76 +55,89 @@ def get_output(dataset, user_id):
 
     if infer_mode:
         _, output = trainer.test(user_id=USER_ID)
-        print(output)   
     
     return user_history, output
 
 
-
 # Streamlit UI
+# Set page configuration
 st.set_page_config(page_title='TAGON',
                    page_icon=":material/sdk:",
-                    layout='centered',
-                    initial_sidebar_state='expanded',
-                    menu_items={
-                        "Get Help": "https://www.github.com/siddhantpathakk/tagon",
-                        'About': "This is a final year project demonstration for TAGON."
-                        }
-                    )
+                   layout='centered',
+                   initial_sidebar_state='expanded',
+                   menu_items={
+                       "Get Help": "https://www.github.com/siddhantpathakk/tagon",
+                       'About': "This is a final year project demonstration for TAGON."
+                   }
+                   )
 
-st.title(f'FYP Demonstration for TAGON')
+st.title('FYP Demonstration for TAGON')
 
-dataset_names = ['ml-100k', 'Baby', 'Digital_Music', 'Toys_and_Games', 'Tools_and_Home_Improvement']
-
-# Sidebar
-st.sidebar.title("Settings")
-
-st.sidebar.subheader("Select dataset and user ID")
-selected_dataset = st.sidebar.selectbox("Select dataset", dataset_names, placeholder="Select a dataset")
-user_id = st.sidebar.text_input("Enter User ID", "2")
-user_id = int(user_id)
-
+# Load user set
 userset_path = 'src/app/userset.json'
 userset_map = json.load(open(userset_path, 'r'))
-valid_user_id_flag = True
-if user_id not in userset_map[selected_dataset]:
+
+# Sidebar UI - Dataset and User ID Selection
+dataset_names = ['ml-100k', 'Baby', 'Digital_Music',
+                 'Toys_and_Games', 'Tools_and_Home_Improvement']
+selected_dataset = st.sidebar.selectbox("Select dataset", dataset_names, index=dataset_names.index(
+    st.session_state.get('selected_dataset', dataset_names[0])))
+default_user_id = userset_map[selected_dataset][0]
+user_id = st.sidebar.text_input("Enter User ID (randomly chosen by default)", str(
+    st.session_state.get('user_id', default_user_id)))
+user_id = int(user_id)
+
+# Validity check for User ID
+valid_user_id_flag = user_id in userset_map[selected_dataset]
+if not valid_user_id_flag:
     st.error(f"User ID {user_id} not found in the dataset")
     st.toast("Please enter a valid User ID", icon="🚫")
-    valid_user_id_flag = False
 
+# Additional Settings
 st.sidebar.subheader("CTBG Settings")
 trim = st.sidebar.slider("Trim (for CTBG)", 1, 20, 4)
 show_csv = st.sidebar.checkbox("Show CSV Data")
 
-session_state = False
-st.sidebar.subheader("Make Predictions")
-predict_button = st.sidebar.button('Predict', type='primary',
-                                   disabled=not valid_user_id_flag)
+# Prediction Button
+# st.sidebar.subverified("Make Predictions")
+predict_button = st.sidebar.button('Predict', disabled=not valid_user_id_flag)
+
+# Process predictions
 if predict_button and valid_user_id_flag:
-    print(f"Selected dataset: {selected_dataset}")
-    print(f"User ID: {user_id}")
-    user_hist, output = get_output(selected_dataset, user_id)
-    session_state = True
-    st.toast("Predictions completed!",  icon="✅")
-    
-if session_state and valid_user_id_flag:
+    with st.spinner("Running information through the model..."):
+        user_hist, output = get_output(selected_dataset, user_id)
+        st.session_state['user_hist'] = user_hist
+        st.session_state['output'] = output
+        st.toast("Predictions completed!", icon="✅")
+
+# Display Results
+if 'output' in st.session_state and valid_user_id_flag:
     st.header("Continuous Time Bipartite Graph (CTBG)")
-    fig = make_ctbg(user_hist, selected_dataset, trim=trim)
+    fig = make_ctbg(st.session_state['user_hist'], selected_dataset, trim=trim,
+                    theme=st.get_option("theme.backgroundColor"))
     st.plotly_chart(fig)
-        
+
     if show_csv:
-        dataframe = pd.DataFrame(user_hist)
-        dataframe = dataframe.sort_values(by='ts', ascending=False)
+        dataframe = pd.DataFrame(st.session_state['user_hist'])
         dataframe['ts'] = pd.to_datetime(dataframe['ts'], unit='s')
-        dataframe = dataframe[['u', 'i', 'ts']]
-        st.dataframe(dataframe)
+        dataframe = dataframe.sort_values(by='ts', ascending=False)
+        dataframe = dataframe[['u', 'i', 'ts']].head(trim)
+        st.plotly_chart(make_plotly_table(dataframe, 'User History'))
 
     st.header("Recommendations")
-    fig2 = make_recgraph(fig, output)
-    st.plotly_chart(fig2)
+    out_dataframe = pd.DataFrame(st.session_state['output']).rename(
+        columns={'u_pos_gd': 'i', 'u_ind': 'u', 'timestamp': 'ts'})
+    out_dataframe['ts'] = pd.to_datetime(out_dataframe['ts'], unit='s')
+    reference_point = st.selectbox(
+        "Choose reference point for next-item prediction", out_dataframe['i'].unique())
+    n_recs = st.slider("Number of recommendations", 1, 20, 5)
+    st.write(f"Top {n_recs} recommendations:")
+    recs = out_dataframe[out_dataframe['i'] ==
+                         reference_point]['predicted'].iloc[0][:n_recs]
+    st.write(' - '.join(str(x) for x in recs))
+
     if show_csv:
-        st.subheader("CSV Data")
-        dataframe = pd.DataFrame(output)
-        dataframe = dataframe.sort_values(by='timestamp', ascending=False)
-        dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'], unit='s')
-        st.dataframe(dataframe)
+        rec_df = pd.merge(dataframe, out_dataframe, on=[
+                          'u', 'ts', 'i'], how='outer').dropna()
+        rec_df['predicted'] = rec_df['predicted'].apply(lambda x: x[:5])
+        st.plotly_chart(make_plotly_table(rec_df, 'Recommendations'))
